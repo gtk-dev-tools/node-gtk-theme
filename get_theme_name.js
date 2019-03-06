4bd828825815b9ba6aae553d0b14337f5864f21a
Jake <jakeryanjarrett@gmail.com>
2019-03-06 18:38:38 +1100

    Initial work to making it so users can have a responsive UI when the theme has changed

--- get_theme_name.js
+++ get_theme_name.js
@@ -1,4 +1,4 @@
 const { execSync } = require('child_process');
 
 // Return a function to get the current theme.
-exports.gtk_theme = execSync('gsettings get org.gnome.desktop.interface gtk-theme', { encoding: 'utf8' });
+exports.gtk_theme = () => execSync('gsettings get org.gnome.desktop.interface gtk-theme', { encoding: 'utf8' });
